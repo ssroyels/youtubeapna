08d3d0c2ea1dcaabfb4f77887e32e7cdaec6bf03
satyam singh <satyam884060singh@gmail.com>
2025-04-29 12:19:52 +0530

    initialized

--- Frontend/src/front/front.jsx
+++ Frontend/src/front/front.jsx
@@ -12,7 +12,7 @@ function Front() {
       <SideBar />
       <div className="h-[calc(100vh-6.625rem)] overflow-y-scroll overflow-x-hidden">
         <Listitem />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
           {!loading &&
             data.map((item) => {
               if (item.type !== "video") return false;
